65bce4e59e750300c8d9f9418bf8990dc4e6a8a4
Zhang Rui <v.me-zr@live.cn>
2014-05-10 23:19:06 +0800

    测试opg分析法程序

---
+++ opg_analysis.cpp
@@ -0,0 +1,37 @@
+// =====================================================================================
+// 
+//       Filename:  opg_analysis.cpp
+//
+//    Description:  算符优先分析法
+//
+//        Version:  1.0
+//        Created:  2014年05月07日 21时14分58秒
+//       Revision:  none
+//       Compiler:  g++
+//
+//         Author:  ZhangRui, v.me-zr@live.cn
+//        Company:  Class 1107 of Computer Science and Technology
+// 
+// =====================================================================================
+
+#include "grammar.h"
+
+int main(int argc, char *argv[])
+{
+	OperatorGrammar opg;
+	opg.input_grammar();
+	if (opg.legal_grammar() == -1) {
+		return -1;
+	}
+	opg.char_terminator();
+	opg.trans_grammar();
+	opg.firstvt();
+	opg.lastvt();
+	if(opg.table() == -1) {
+		return -1;
+	}
+	opg.deal();
+
+	return 0;
+}
+
